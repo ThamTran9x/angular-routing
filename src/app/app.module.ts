94537fd40148fd1f48b02654c0670ccc2166409f
tham tran <thamtran9x@gmail.com>
2020-07-23 00:03:23 +0200

    practice with params and fragment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,14 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { from } from 'rxjs';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'users', component: UsersComponent },
   { path: 'users/:id/:name', component: UserComponent },
   { path: 'servers', component: ServersComponent },
+  { path: 'servers/:id', component: ServerComponent },
+
   { path: 'servers/:id/edit', component: EditServerComponent },
 
 
